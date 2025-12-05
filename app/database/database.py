e4435c52f5dbb176f3f922486f7ce9749aade630
basirdez <basirdez@gmail.com>
2025-12-05 10:53:24 +0330

    bugfix

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, inspect
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 import argparse
 
@@ -15,6 +14,13 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def init_db():
